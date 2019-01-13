7f5df0737cbd0030301c703a81da2820f1ad2aec
Robert Siipola <robert.siipola@futurice.com>
2019-01-13 15:57:29 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='TPractice task for the application of transfer learning in computer vision tasks',
+    author='Robert Siipola',
+    license='MIT',
+)
